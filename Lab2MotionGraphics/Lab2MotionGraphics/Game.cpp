dba3b2cc1378c752b69ac51810b63e8f224900a5
Ryan Holloway <c00283423@setu.ie>
2024-01-25 16:05:12 +0000

    level editor with basic map

---
+++ Lab2MotionGraphics/Lab2MotionGraphics/Game.cpp
@@ -0,0 +1,163 @@
+/// <summary>
+/// @author Peter Lowe
+/// @date May 2019
+///
+/// you need to change the above lines or lose marks
+/// </summary>
+
+#include "Game.h"
+#include <iostream>
+
+
+
+/// <summary>
+/// default constructor
+/// setup the window properties
+/// load and setup the text 
+/// load and setup thne image
+/// </summary>
+Game::Game() :
+	m_window{ sf::VideoMode{ 800U, 800U, 32U }, "SFML Game" },
+	m_exitGame{false} //when true game will exit
+{
+	init();
+}
+
+/// <summary>
+/// default destructor we didn't dynamically allocate anything
+/// so we don't need to free it, but mthod needs to be here
+/// </summary>
+Game::~Game()
+{
+}
+
+
+/// <summary>
+/// main game loop
+/// update 60 times per second,
+/// process update as often as possible and at least 60 times per second
+/// draw as often as possible but only updates are on time
+/// if updates run slow then don't render frames
+/// </summary>
+void Game::run()
+{	
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	const float fps{ 60.0f };
+	sf::Time timePerFrame = sf::seconds(1.0f / fps); // 60 fps
+	while (m_window.isOpen())
+	{
+		processEvents(); // as many as possible
+		timeSinceLastUpdate += clock.restart();
+		while (timeSinceLastUpdate > timePerFrame)
+		{
+			timeSinceLastUpdate -= timePerFrame;
+			processEvents(); // at least 60 fps
+			update(timePerFrame); //60 fps
+		}
+		render(); // as many as possible
+	}
+}
+/// <summary>
+/// handle user and system events/ input
+/// get key presses/ mouse moves etc. from OS
+/// and user :: Don't do game update here
+/// </summary>
+void Game::processEvents()
+{
+	sf::Event newEvent;
+	while (m_window.pollEvent(newEvent))
+	{
+		if ( sf::Event::Closed == newEvent.type) // window message
+		{
+			m_exitGame = true;
+		}
+		if (sf::Event::KeyPressed == newEvent.type) //user pressed a key
+		{
+			processKeys(newEvent);
+		}
+	}
+}
+
+
+/// <summary>
+/// deal with key presses from the user
+/// </summary>
+/// <param name="t_event">key press event</param>
+void Game::processKeys(sf::Event t_event)
+{
+	if (sf::Keyboard::Escape == t_event.key.code)
+	{
+		m_exitGame = true;
+	}
+}
+
+/// <summary>
+/// Update the game world
+/// </summary>
+/// <param name="t_deltaTime">time interval per frame</param>
+void Game::update(sf::Time t_deltaTime)
+{
+	for (int index = 0; index < amountOfTiles; index++)
+	{
+		tiles[index].setPosition(sf::Vector2f(tiles[index].getPosition().x, tiles[index].getPosition().y-1 ));
+	}
+	if (m_exitGame)
+	{
+		m_window.close();
+	}
+}
+
+/// <summary>
+/// draw the frame and then switch buffers
+/// </summary>
+void Game::render()
+{
+	m_window.clear(sf::Color::Black);
+	for (int index = 0; index < amountOfTiles; index++)
+	{
+		m_window.draw(tiles[index]);
+	}
+	m_window.display();
+}
+
+void Game::setupTiles()
+{
+	for (int index = 0; index < amountOfTiles; index++)
+	{
+		
+		tiles[index].setSize(sf::Vector2f(80.0f, 80.0f));
+		tiles[index].setPosition(column * 80, row*80);
+		column++;
+
+		if (column == 10)
+		{
+			column = 0;
+			row++;
+		}
+	
+		if (levelData[index] == 1)
+		{
+			tiles[index].setFillColor(sf::Color::Red);
+
+		}
+		else
+		{
+			tiles[index].setFillColor(sf::Color::Transparent);
+		}
+	}
+
+}
+
+/// <summary>
+/// load the font and setup the text message for screen
+/// </summary>
+void Game::init()
+{
+	if (!m_ArialBlackfont.loadFromFile("ASSETS\\FONTS\\ariblk.ttf"))
+	{
+		std::cout << "problem loading arial black font" << std::endl;
+	}
+	setupTiles();
+
+}
---
+++ Lab2MotionGraphics/Lab2MotionGraphics/Game.h
@@ -0,0 +1,73 @@
+/// <summary>
+/// author Pete Lowe May 2019
+/// you need to change the above line or lose marks
+/// </summary>
+#ifndef GAME_HPP
+#define GAME_HPP
+/// <summary>
+/// include guards used so we don't process this file twice
+/// same as #pragma once
+/// Don't forget the endif at the bottom
+/// </summary>
+#include <SFML/Graphics.hpp>
+const int amountOfTiles = 200;
+class Game
+{
+public:
+	Game();
+	~Game();
+	/// <summary>
+	/// main method for game
+	/// </summary>
+	void run();
+
+private:
+
+	void processEvents();
+	void processKeys(sf::Event t_event);
+	void update(sf::Time t_deltaTime);
+	void render();
+
+	void setupTiles();
+	
+	void init();
+
+	sf::RenderWindow m_window; // main SFML window
+	sf::Font m_ArialBlackfont; // font used by message
+
+	
+
+	sf::RectangleShape tiles[amountOfTiles];
+	int row = 0;
+	int column = 0;
+	int levelData[amountOfTiles]
+	{
+		1,1,1,1,0,0,0,1,1,1,
+		1,1,1,0,0,1,1,1,1,1,
+		1,1,0,0,1,1,1,1,1,1,
+		1,1,1,0,0,1,1,1,1,1,
+		1,1,0,0,0,1,1,1,1,1,
+		1,1,1,0,0,0,1,1,1,1,
+		1,1,1,1,0,0,0,1,1,1,
+		1,1,1,1,0,0,1,1,1,1,
+		1,1,1,0,0,0,1,1,1,1,
+		1,1,0,0,0,1,1,1,1,1,
+
+		1,1,1,1,0,0,0,1,1,1,
+		1,1,1,0,0,1,1,1,1,1,
+		1,1,0,0,1,1,1,1,1,1,
+		1,1,1,0,0,1,1,1,1,1,
+		1,1,0,0,0,1,1,1,1,1,
+		1,1,1,0,0,0,1,1,1,1,
+		1,1,1,1,0,0,0,1,1,1,
+		1,1,1,1,0,0,1,1,1,1,
+		1,1,1,0,0,0,1,1,1,1,
+		1,1,0,0,0,1,1,1,1,1
+	};
+
+	bool m_exitGame; // control exiting game
+
+};
+
+#endif // !GAME_HPP
+
